e4a66c474ebbf49647cc5d1a298a26c991fc7928
lakshan chathura <37142357+lakshan-chathura@users.noreply.github.com>
2022-07-04 11:28:07 +0530

    update some line of code

--- BubbleSort.java
+++ BubbleSort.java
@@ -18,16 +18,15 @@ static void bubbleSort(int arr[]){
         isSorted=true;    //Code Optimization
         for(int j=1;j<arr.length-i;j++){
             if(arr[j]<arr[j-1]){
-                temp=arr[j-1];
-                arr[j-1]=arr[j];
-                arr[j]=temp;
+                temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
                isSorted=false;
             }
         }
-     if(isSorted){
+        if(isSorted){
             return;
-     }
-        
+        }
     }
 }
    
